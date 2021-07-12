28ca0f3b24325e550e01399cc0faf56699f594be
Ryan.bennis <Buddybot180@gmail.com>
2021-07-12 19:51:22 -0400

    added book class

---
+++ MidtermProject/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace MidtermProject
+{
+   public class Book
+    {
+
+
+        public static void CreateFile(string fileName)
+        {
+            if (!File.Exists(fileName))
+            {
+                File.Create(fileName);
+            }
+
+
+        }
+
+
+    }
+}
