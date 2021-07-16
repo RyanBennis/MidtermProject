725c084f684f62162505548f968d4d8d36e8c51e
larissa <larissaliane1@gmail.com>
2021-07-15 17:38:27 -0700

    adding in userinput and search classes

--- MidtermProject/Program.cs
+++ MidtermProject/Program.cs
@@ -1,42 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.CompilerServices;
-
 namespace MidtermProject
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //Welcome Screen, under construction 
-            // Console.WriteLine("Welcome to our Library What would you like to do?");
-            // Console.WriteLine("");
-
-            var libraryName = "CurrentBookList.txt";
-            Book.CreateFile(libraryName);
-
-
-
-
-
-            var library = new Book();
-            var books = library.DisplayLibrary();
-            foreach (var book in books)
-            {
-                Console.WriteLine($"{book.Title} by {book.Author}");
-
-            }
-
-            Console.WriteLine("This is the work I've been doing");
-
-            var test = new List<Book>(library.DisplayLibrary());
-
-           var libraryContents = Book.UpdateFile(libraryName, test);
-
             string continueFlag;
             Console.WriteLine("Welcome to our Library, We are happy you are here");
 
+
+            var libraryName = "CurrentBookList.txt";
+            FileIO.CreateFile(libraryName);
             do
             {
                 Console.WriteLine("Here are the books that we have in our Library");
@@ -45,75 +21,26 @@ namespace MidtermProject
                 foreach (var book in books)
                 {
                     Console.WriteLine($"{book.Title} by {book.Author}");
-                    //if (book.InLibrary == true)
-                    //{
-                    //    Console.WriteLine("This book is available right now and available to be checked out");
-                    //}
-                    //else
-                    //{
-                    //    Console.WriteLine($"This book is currently checked out and will be available {book.DueDate}");
-                    //}
                 }
-                Titles(books);
-                continueFlag = GetUserInput("Do you want to continue (y/n)");
+                var userInput = new UserInput();
+                string searchAuthor = userInput.GetUserInput("Please enter the name of a book or an Author?");
+
+                var searchMethod = new SearchMethod();
+                var searchedBooks = searchMethod.Titles(books, searchAuthor);
+                var userInput1 = new UserInput();
+                continueFlag = userInput1.GetUserInput("Do you want to continue (y/n)");
             } while (continueFlag == "y");
-        }
 
-            static string GetUserInput(string message)
-            {
-                string input;
-                do
-                {
-                    // print message
-                    Console.WriteLine(message);
-                    // get input
-                    input = Console.ReadLine();
-                    if (string.IsNullOrWhiteSpace(input))
-                    {
-                        Console.WriteLine("Invalid input");
-                    }
-                    // if invalid go back to top
-                } while (string.IsNullOrWhiteSpace(input));
-                return input;
-            }
-        
-       
 
-           //public static void List(List<Book> books)
-           // {
-           //     //var results1 = moviess;
-           //     foreach (var book in books)
-           //     {
-           //         Console.WriteLine($"{book.Title}");
-           //     }
-           // }
-            public static void Titles(List<Book> books)
-            {
-                //Console.WriteLine("What category are you interested in?");
-                //string input = Console.ReadLine();
-                foreach (var bookList in books)
-                {
-                    Console.WriteLine($"{bookList.Title}:{bookList.Author}");
-                }
+            //Console.WriteLine("This is the work I've been doing");
+            //var test = new List<Book>(library.DisplayLibrary());
+            //var libraryContents = FileIO.UpdateFile(libraryName, test);
 
-                string input = GetUserInput("What category are you interested in?");
-                var results = books.Where(book => book.Author == input);
-
-                foreach (var book in results)
-                {
-                    Console.WriteLine($"{book.Title}: {book.InLibrary}: {book.DueDate}");
-                }
-
-            }
-            //Book.UpdateFile(fileName, listOfBooks);
             // Book.UpdateFile(fileName, listOfBooks);
-
             ////Read
             //Book.ReadFromFile(fileName);
-
             //Delete
-          //  Book.DeleteFile(libraryName);
-
-        
+            //  Book.DeleteFile(libraryName);
+        }
     }
 }
---
+++ MidtermProject/SearchMethod.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace MidtermProject
+{
+    public class SearchMethod
+    {
+
+        public List<Book> Titles(List<Book> books, string input)
+        {
+            
+            var results = books.Where(book => book.Title == input | book.Author == input).ToList();
+
+
+            foreach (var book in results)
+            {
+                if (input == book.Title | input == book.Author && book.InLibrary == true)
+                {
+                    Console.WriteLine($" Thank you for checking out {book.Title}, please return the book on {book.DueDate.AddDays(14)}");
+                }
+                else if (input == book.Title | input == book.Author && book.InLibrary == false)
+                {
+                    Console.WriteLine($"{book.Title} is not available, it will be returned on {book.DueDate}");
+                }
+            }
+            return results;
+        }
+    }
+}
